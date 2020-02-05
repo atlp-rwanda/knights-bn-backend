93eafbf4bdfab22c22c2c0e48a412bb1f55d4f87
Kagorora <kagororamaxime@gmail.com>
2020-02-05 23:18:42 +0200

    ch(sequelize): configure sequelize
    - configure sequelize
    - configure postgres
    
    [Start: #170947644 #170947645]

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import dotenv from 'dotenv';
 import app from './app';
 
 dotenv.config();
+app.use((req, res) => {
+  res.status(404).send({
+    status: 404,
+    error: 'Not Found!',
+  });
+});
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => { console.log(`listening on port ${port} ...`); });
+
+export default app;
