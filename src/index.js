678c6836d12b67aa26e3f9b6da14b9a5c39e64d3
EugeneMunya <eugenemu57@gmail.com>
2020-02-06 12:01:43 +0200

    chore(dockerfile):create dockerfile - Add dockerfile for creating docker image [Start #170947653]

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import dotenv from 'dotenv';
 import app from './app';
 
 dotenv.config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => { console.log(`listening on port ${port} ...`); });
